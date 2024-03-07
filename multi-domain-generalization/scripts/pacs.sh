302efe551f2bfd755d4ee44f114cfce80465d660
nduongw <duong5112001@gmail.com>
2024-03-07 19:45:29 +0700

    update

--- multi-domain-generalization/scripts/pacs.sh
+++ multi-domain-generalization/scripts/pacs.sh
@@ -8,16 +8,16 @@ D4=sketch
 SEED=42
 method=uncertainty
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D2} ${D3} ${D4} \
---target-domains ${D1} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D1})
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D2} ${D3} ${D4} \
+# --target-domains ${D1} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D1})
 
 # (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
 # --root ${DATA} \
@@ -41,16 +41,16 @@ method=uncertainty
 # --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
 # --output-dir output/dg/${DATASET}/${method}/${D3})
 
-# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
-# --root ${DATA} \
-# --trainer Vanilla \
-# --uncertainty 0.5 \
-# --source-domains ${D1} ${D2} ${D3} \
-# --target-domains ${D4} \
-# --seed ${SEED} \
-# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
-# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
-# --output-dir output/dg/${DATASET}/${method}/${D4})
+(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+--root ${DATA} \
+--trainer Vanilla \
+--uncertainty 0.5 \
+--source-domains ${D1} ${D2} ${D3} \
+--target-domains ${D4} \
+--seed ${SEED} \
+--dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+--config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+--output-dir output/dg/${DATASET}/${method}/${D4})
 
 echo "Running scripts in parallel"
 wait # This will wait until both scripts finish
