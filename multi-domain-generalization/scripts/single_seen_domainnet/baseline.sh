c7b638247bf851bff10a9bcb2dd63a3dbab4db8f
nduongw <duong5112001@gmail.com>
2024-04-21 10:36:20 +0700

    update scripts

--- multi-domain-generalization/scripts/single_seen_domainnet/baseline.sh
+++ multi-domain-generalization/scripts/single_seen_domainnet/baseline.sh
@@ -10,57 +10,57 @@ D6=sketch
 SEED=42
 method=baseline
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D1} \
---target-domains ${D6} ${D2} ${D3} ${D4} ${D5} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D6}_${D2}_${D3}_${D4}_${D5} \
---reduce 1 \
---resume false)
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D1} \
+# --target-domains ${D6} ${D2} ${D3} ${D4} ${D5} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D6}_${D2}_${D3}_${D4}_${D5} \
+# --reduce 1 \
+# --resume false)
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D2} \
---target-domains ${D1} ${D6} ${D3} ${D4} ${D5} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D1}_${D6}_${D3}_${D4}_${D5} \
---reduce 1 \
---resume false)
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D2} \
+# --target-domains ${D1} ${D6} ${D3} ${D4} ${D5} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D1}_${D6}_${D3}_${D4}_${D5} \
+# --reduce 1 \
+# --resume false)
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D3} \
---target-domains ${D1} ${D2} ${D6} ${D4} ${D5} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D6}_${D4}_${D5} \
---reduce 1 \
---resume false)
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D3} \
+# --target-domains ${D1} ${D2} ${D6} ${D4} ${D5} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D6}_${D4}_${D5} \
+# --reduce 1 \
+# --resume false)
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D4} \
---target-domains ${D1} ${D2} ${D3} ${D6} ${D5} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D3}_${D6}_${D5} \
---reduce 1 \
---resume false)
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D4} \
+# --target-domains ${D1} ${D2} ${D3} ${D6} ${D5} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D3}_${D6}_${D5} \
+# --reduce 1 \
+# --resume false)
 
 (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
 --root ${DATA} \
@@ -75,18 +75,18 @@ method=baseline
 --reduce 1 \
 --resume false)
 
-(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
---root ${DATA} \
---trainer Vanilla \
---uncertainty 0.5 \
---source-domains ${D6} \
---target-domains ${D1} ${D2} ${D3} ${D4} ${D5} \
---seed ${SEED} \
---dataset-config-file configs/datasets/dg/${DATASET}.yaml \
---config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
---output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D3}_${D4}_${D5} \
---reduce 1 \
---resume false)
+# (CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+# --root ${DATA} \
+# --trainer Vanilla \
+# --uncertainty 0.5 \
+# --source-domains ${D6} \
+# --target-domains ${D1} ${D2} ${D3} ${D4} ${D5} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}.yaml \
+# --config-file configs/trainers/dg/vanilla/${DATASET}.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D1}_${D2}_${D3}_${D4}_${D5} \
+# --reduce 1 \
+# --resume false)
 
 echo "Running scripts in parallel"
 wait # This will wait until both scripts finish
