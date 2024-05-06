5dd8ba5472c4ff46a590b0854ee8b7995e9678ff
nduongw <duong5112001@gmail.com>
2024-05-06 17:44:13 +0700

    update config

---
+++ multi-domain-generalization/scripts/cifar10c/conststyle2.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+DATA=./DATA
+DATASET=cifar10_c
+D1=cifar10
+D2=cifar10_c
+SEED=42
+method=conststyle
+
+# (CUDA_VISIBLE_DEVICES=1 python tools/train.py \
+# --root ${DATA} \
+# --trainer ConstStyleTrainer \
+# --uncertainty 0.5 \
+# --source-domains ${D1} \
+# --target-domains ${D2} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}_1_cs.yaml \
+# --config-file configs/trainers/dg/vanilla/cifar10.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D2} \
+# --resume false)
+
+# (CUDA_VISIBLE_DEVICES=1 python tools/train.py \
+# --root ${DATA} \
+# --trainer ConstStyleTrainer \
+# --uncertainty 0.5 \
+# --source-domains ${D1} \
+# --target-domains ${D2} \
+# --seed ${SEED} \
+# --dataset-config-file configs/datasets/dg/${DATASET}_2_cs.yaml \
+# --config-file configs/trainers/dg/vanilla/cifar10.yaml \
+# --output-dir output/dg/${DATASET}/${method}/${D2} \
+# --resume false)
+
+(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+--root ${DATA} \
+--trainer ConstStyleTrainer \
+--uncertainty 0.5 \
+--source-domains ${D1} \
+--target-domains ${D2} \
+--seed ${SEED} \
+--dataset-config-file configs/datasets/dg/${DATASET}_3_cs.yaml \
+--config-file configs/trainers/dg/vanilla/cifar10.yaml \
+--output-dir output/dg/${DATASET}/${method}/${D2} \
+--resume false)
+
+(CUDA_VISIBLE_DEVICES=0 python tools/train.py \
+--root ${DATA} \
+--trainer ConstStyleTrainer \
+--uncertainty 0.5 \
+--source-domains ${D1} \
+--target-domains ${D2} \
+--seed ${SEED} \
+--dataset-config-file configs/datasets/dg/${DATASET}_4_cs.yaml \
+--config-file configs/trainers/dg/vanilla/cifar10.yaml \
+--output-dir output/dg/${DATASET}/${method}/${D2} \
+--resume false)
+
+(CUDA_VISIBLE_DEVICES=1 python tools/train.py \
+--root ${DATA} \
+--trainer ConstStyleTrainer \
+--uncertainty 0.5 \
+--source-domains ${D1} \
+--target-domains ${D2} \
+--seed ${SEED} \
+--dataset-config-file configs/datasets/dg/${DATASET}_5_cs.yaml \
+--config-file configs/trainers/dg/vanilla/cifar10.yaml \
+--output-dir output/dg/${DATASET}/${method}/${D2} \
+--resume false)
+
+echo "Running scripts in parallel"
+wait # This will wait until both scripts finish
+echo "Script done running"
